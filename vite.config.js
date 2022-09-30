6cbe8a2da27800bfb13cbc7c63a5aae683162fde
Charlie Moore <cjm7324@gmail.com>
2022-09-29 22:58:58 -0500

    Bumped up site warning. Explicitly set button text color to black

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ygo-validator-web/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 3000,
+  }
 })
